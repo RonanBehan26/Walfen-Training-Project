24fa2f0978d6b6c4d33713891597fbd0b89b652a
Miguel Angel Ruiz <miguel.ruiz@walfen.com>
2021-04-18 11:11:01 +0200

    Initial commit

---
+++ portal/src/test/java/com/walfen/training/api/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.walfen.training.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
