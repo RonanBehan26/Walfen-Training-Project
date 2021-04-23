22baef47ce58c8e7269d691a2e6f0ab21723ca31
Ronan <Ronan@Ronan.Home>
2021-04-23 10:01:18 +0100

    TA-12 Create Dao Methods and test them.

--- portal/src/main/java/com/walfen/training/api/daos/EmployeeDao.java
+++ portal/src/main/java/com/walfen/training/api/daos/EmployeeDao.java
@@ -9,6 +9,24 @@ import com.walfen.training.api.entities.Employee;
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
 
 	List<Employee> findAllByOrderByPosition();
+
+	List<Employee> findAllByOrderByFirstName();
+
+	List<Employee> findAllByOrderByLastName();
+
+	List<Employee> findAllByOrderByCompany();
+
+	List<Employee> findAllByOrderById();
+
+	List<Employee> findAllByOrderByPositionDesc();
+
+	List<Employee> findAllByOrderByFirstNameDesc();
+
+	List<Employee> findAllByOrderByLastNameDesc();
+
+	List<Employee> findAllByOrderByCompanyDesc();
+
+	List<Employee> findAllByOrderByIdDesc();
 }
 
 
--- portal/src/test/java/com/walfen/training/api/daos/EmployeeTestDao.java
+++ portal/src/test/java/com/walfen/training/api/daos/EmployeeTestDao.java
@@ -25,6 +25,7 @@ public class EmployeeTestDao {
 		assertEquals(total, 3);
 	}
 	
+	//create test order by position, test was successful
 	@Test
 	@Sql(scripts = { "classpath:db/sql/all.sql" })
 	public void testOrderByPosition() throws Exception {
@@ -34,16 +35,103 @@ public class EmployeeTestDao {
 		assertEquals(employees.get(1).getId(), 3);
 		assertEquals(employees.get(2).getId(), 1);
 	}
-	
-	// TODO: create test order by first name
-	// TODO: create test order by last name
-	// TODO: create test order by company
-	// TODO: create test order by id
-	
-	// TODO: create test order by position descending
-	// TODO: create test order by first name descending
-	// TODO: create test order by last name descending
-	// TODO: create test order by company descending
-	// TODO: create test order by id descending
 
+	// Create test order by first name, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByFirstName() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByFirstName();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 3);
+		assertEquals(employees.get(1).getId(), 1);
+		assertEquals(employees.get(2).getId(), 2);
+	}
+	
+	// Create test order by last name, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByLastName() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByLastName();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 1);
+		assertEquals(employees.get(1).getId(), 3);
+		assertEquals(employees.get(2).getId(), 2);
+	}
+	
+	// Create test order by company, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByCompany() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByCompany();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 2);
+		assertEquals(employees.get(1).getId(), 3);
+		assertEquals(employees.get(2).getId(), 1);
+	}
+	
+	// Create test order by id, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderById() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderById();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 1);
+		assertEquals(employees.get(1).getId(), 2);
+		assertEquals(employees.get(2).getId(), 3);
+	}
+	
+	// Create test order by position descending, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByPositionDesc() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByPositionDesc();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 1);
+		assertEquals(employees.get(1).getId(), 3);
+		assertEquals(employees.get(2).getId(), 2);
+	}
+	
+	// Create test order by first name descending, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByFirstNameDesc() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByFirstNameDesc();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 2);
+		assertEquals(employees.get(1).getId(), 1);
+		assertEquals(employees.get(2).getId(), 3);
+	}
+	
+	// TODO: create test order by last name descending, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByLastNameDesc() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByLastNameDesc();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 2);
+		assertEquals(employees.get(1).getId(), 3);
+		assertEquals(employees.get(2).getId(), 1);
+	}
+	
+	// TODO: create test order by company descending, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByCompanyDesc() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByCompanyDesc();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 1);
+		assertEquals(employees.get(1).getId(), 3);
+		assertEquals(employees.get(2).getId(), 2);
+	}
+	
+	// TODO: create test order by id descending, test was successful
+	@Test
+	@Sql(scripts = { "classpath:db/sql/all.sql" })
+	public void testOrderByIdDesc() throws Exception {
+		List<Employee> employees = employeeDao.findAllByOrderByIdDesc();
+		assertEquals(employees.size(), 3);
+		assertEquals(employees.get(0).getId(), 3);
+		assertEquals(employees.get(1).getId(), 2);
+		assertEquals(employees.get(2).getId(), 1);
+	}
 }
