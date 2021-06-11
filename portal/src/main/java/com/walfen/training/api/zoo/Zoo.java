f2a5bca6993685746f8b7ee6db679acce44ba201
Ronan <Ronan@Ronan>
2021-06-11 15:11:48 +0100

    TA-21-improve-zoo-update 2

--- portal/src/main/java/com/walfen/training/api/zoo/Zoo.java
+++ portal/src/main/java/com/walfen/training/api/zoo/Zoo.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Zoo {
-	
-	
+
 	private String name;
 	private String city;
 	private List<Animal> animals = new ArrayList<>();
-	
+
 	public Zoo() {
 		super();
 		this.animals = new ArrayList<>();
@@ -22,41 +21,28 @@ public class Zoo {
 		this.animals = animals;
 	}
 
-//	public Zoo() {
-//		Zebra zebraOne = new Zebra();
-//		animals.add(zebraOne);
-//		Shark sharkOne = new Shark();
-//		animals.add(sharkOne);
-//		Shark sharkTwo = new Shark();
-//		animals.add(sharkTwo);
-//		Tiger tigerOne = new Tiger();
-//		animals.add(tigerOne);
-//		Tiger tigerTwo = new Tiger();
-//		animals.add(tigerTwo);
-//	}
-	
 	public List<Animal> getAnimals() {
-	        return this.animals;
-	    }
-	
+		return this.animals;
+	}
+
 	public int getAnimalsSize() {
-        return this.animals.size();
-    }
-	
-	
-	
+		return this.animals.size();
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
 
 }
-
